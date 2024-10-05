c5c39d7d17210f416538abcf71f5b1da5ecb7809
Israel López <ialopezm@indra.es>
2024-10-05 16:15:29 +0200

    Revert "commit"
    This reverts commit 1c6c64a59ba7e7fcaee4ae90e28e02df6fd89cd6.

---
+++ include/saurion.hpp
@@ -0,0 +1,37 @@
+#ifndef SAURION_HPP
+#define SAURION_HPP
+
+#include "low_saurion.h"
+
+class Saurion {
+ public:
+  using ConnectedCb = void (*)(const int, void *);
+  using ReadedCb = void (*)(const int, const void *const, const ssize_t, void *);
+  using WroteCb = void (*)(const int, void *);
+  using ClosedCb = void (*)(const int, void *);
+  using ErrorCb = void (*)(const int, const char *const, const ssize_t, void *);
+
+  explicit Saurion(const uint32_t thds, const int sck) noexcept;
+  ~Saurion();
+
+  Saurion(const Saurion &) = delete;
+  Saurion(Saurion &&) = delete;
+  Saurion &operator=(const Saurion &) = delete;
+  Saurion &operator=(Saurion &&) = delete;
+
+  void init() noexcept;
+  void stop() noexcept;
+
+  Saurion *on_connected(ConnectedCb ncb, void *arg) noexcept;
+  Saurion *on_readed(ReadedCb ncb, void *arg) noexcept;
+  Saurion *on_wrote(WroteCb ncb, void *arg) noexcept;
+  Saurion *on_closed(ClosedCb ncb, void *arg) noexcept;
+  Saurion *on_error(ErrorCb ncb, void *arg) noexcept;
+
+  void send(const int fd, const char *const msg) noexcept;
+
+ private:
+  struct saurion *s;
+};
+
+#endif  // !SAURION_HPP
