88faa44d7fff854b684c04e12e18b3c6d7e612aa
Israel López <israel.lopez.developer@gmail.com>
2024-10-22 15:13:39 +0200

    commit

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,34 +1,45 @@
+/*!
+ * @defgroup ThreadPool
+ *
+ * @{
+ */
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
 #include <stddef.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-struct task;
+  struct task;
 
-struct threadpool;
+  struct threadpool;
 
-struct threadpool *threadpool_create(size_t num_threads);
+  struct threadpool *threadpool_create (size_t num_threads);
 
-struct threadpool *threadpool_create_default(void);
+  struct threadpool *threadpool_create_default (void);
 
-void threadpool_init(struct threadpool *pool);
+  void threadpool_init (struct threadpool *pool);
 
-void threadpool_add(struct threadpool *pool, void (*function)(void *), void *argument);
+  void threadpool_add (struct threadpool *pool, void (*function) (void *),
+                       void *argument);
 
-void threadpool_stop(struct threadpool *pool);
+  void threadpool_stop (struct threadpool *pool);
 
-int threadpool_empty(struct threadpool *pool);
+  int threadpool_empty (struct threadpool *pool);
 
-void threadpool_wait_empty(struct threadpool *pool);
+  void threadpool_wait_empty (struct threadpool *pool);
 
-void threadpool_destroy(struct threadpool *pool);
+  void threadpool_destroy (struct threadpool *pool);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif  // !THREADPOOL_H
+#endif // !THREADPOOL_H
+
+/*!
+ * @}
+ */
